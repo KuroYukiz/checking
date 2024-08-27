5546b58d3e3cc9a0665ffcf3ef3a062e872076a8
Alexandra Phillips <alexandraphillips854@gmail.com>
2024-08-27 01:21:58 -1200

    for me

---
+++ class.js
@@ -0,0 +1,14 @@
+console.log(yuki);
+//     console.log(typeof "hello world");
+//     console.log(typeof ["alex", "phillips", "26"]);
+//     console.log(typeof {name:"alexandra phillips", age:"26"});
+//     console.log(typeof true, "or", false);
+//     console.log(typeof null)
+//     console.log(typeof undefined)
+
+
+let kuro;
+
+kuro = "go all the way";
+
+let yuki = "dfdfvv";
